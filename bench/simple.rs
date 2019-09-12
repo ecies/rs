fbd6065d2fa259e1db63c459d7ea70bb6e08bf28
Weiliang Li <to.be.impressive@gmail.com>
2019-09-12 10:45:44 +0900

    Bench (#4)
    * [Add] bench
    
    * [Fix] circle test

---
+++ bench/simple.rs
@@ -0,0 +1,29 @@
+use core::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use ecies::{decrypt, encrypt, utils::generate_keypair};
+
+const BIG_MSG_SIZE: usize = 100 * 1024 * 1024;
+const BIG_MSG: [u8; BIG_MSG_SIZE] = [1u8; BIG_MSG_SIZE];
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let (sk, pk) = generate_keypair();
+    let (sk, pk) = (&sk[..], &pk.serialize_uncompressed());
+
+    let encrypted = &encrypt(pk, &BIG_MSG).unwrap();
+
+    c.bench_function("encrypt 100M", |b| {
+        b.iter(|| encrypt(pk, &BIG_MSG).unwrap())
+    });
+
+    c.bench_function("decrypt 100M", |b| {
+        b.iter(|| decrypt(sk, encrypted).unwrap())
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10).measurement_time(Duration::new(20, 0));
+    targets = criterion_benchmark
+}
+criterion_main!(benches);
