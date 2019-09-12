9aeeeca28257464e51137c248d06ad9a39d1e2f1
Weiliang Li <to.be.impressive@gmail.com>
2019-09-12 14:48:47 +0900

    [Fix] use global context instead of creating context inside function

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
 use hex::decode;
 use hkdf::Hkdf;
+use lazy_static::lazy_static;
 use openssl::symm::{decrypt_aead, encrypt_aead, Cipher};
-use rand::{rngs::OsRng, RngCore};
-use secp256k1::{constants::UNCOMPRESSED_PUBLIC_KEY_SIZE, PublicKey, Secp256k1, SecretKey};
+use rand::{thread_rng, Rng};
+use secp256k1::{constants::UNCOMPRESSED_PUBLIC_KEY_SIZE, All, PublicKey, Secp256k1, SecretKey};
 use sha2::Sha256;
 
 const AES_IV_LENGTH: usize = 16;
@@ -10,22 +11,12 @@ const AES_TAG_LENGTH: usize = 16;
 const AES_IV_PLUS_TAG_LENGTH: usize = AES_IV_LENGTH + AES_TAG_LENGTH;
 const EMPTY_BYTES: [u8; 0] = [];
 
-pub fn get_valid_secret() -> SecretKey {
-    let mut key = [0u8; 32];
-
-    loop {
-        OsRng.fill_bytes(&mut key);
-        if let Ok(k) = SecretKey::from_slice(&key) {
-            return k;
-        }
-    }
+lazy_static! {
+    static ref CONTEXT: Secp256k1<All> = Secp256k1::new();
 }
 
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
-    let secp = Secp256k1::new();
-    let sk = get_valid_secret();
-    let pk = PublicKey::from_secret_key(&secp, &sk);
-    (sk, pk)
+    CONTEXT.generate_keypair(&mut thread_rng())
 }
 
 pub fn remove0x(hex: &str) -> &str {
@@ -40,51 +31,39 @@ pub fn decode_hex(hex: &str) -> Vec<u8> {
 }
 
 pub fn encapsulate(sk: &SecretKey, peer_pk: &PublicKey) -> [u8; 32] {
-    let secp = Secp256k1::new();
     let mut shared_point = *peer_pk;
-    shared_point.mul_assign(&secp, &sk[..]).unwrap();
+    shared_point.mul_assign(&CONTEXT, &sk[..]).unwrap();
 
     let mut master = Vec::with_capacity(UNCOMPRESSED_PUBLIC_KEY_SIZE * 2);
     master.extend(
-        PublicKey::from_secret_key(&secp, sk)
+        PublicKey::from_secret_key(&CONTEXT, sk)
             .serialize_uncompressed()
             .iter(),
     );
     master.extend(shared_point.serialize_uncompressed().iter());
-
-    let h = Hkdf::<Sha256>::new(None, master.as_slice());
-    let mut out = [0u8; 32];
-    h.expand(&[], &mut out).unwrap();
-
-    out
+    hkdf_sha256(master.as_slice())
 }
 
 pub fn decapsulate(pk: &PublicKey, peer_sk: &SecretKey) -> [u8; 32] {
-    let secp = Secp256k1::new();
     let mut shared_point = *pk;
-    shared_point.mul_assign(&secp, &peer_sk[..]).unwrap();
+    shared_point.mul_assign(&CONTEXT, &peer_sk[..]).unwrap();
 
     let mut master = Vec::with_capacity(UNCOMPRESSED_PUBLIC_KEY_SIZE * 2);
     master.extend(pk.serialize_uncompressed().iter());
     master.extend(shared_point.serialize_uncompressed().iter());
 
-    let h = Hkdf::<Sha256>::new(None, master.as_slice());
-    let mut out = [0u8; 32];
-    h.expand(&[], &mut out).unwrap();
-
-    out
+    hkdf_sha256(master.as_slice())
 }
 
 pub fn aes_encrypt(key: &[u8], msg: &[u8]) -> Vec<u8> {
     let cipher = Cipher::aes_256_gcm();
 
     let mut iv = [0u8; AES_IV_LENGTH];
-    OsRng.fill_bytes(&mut iv);
+    thread_rng().fill(&mut iv);
 
     let mut tag = [0u8; AES_TAG_LENGTH];
 
     let encrypted = encrypt_aead(cipher, key, Some(&iv), &EMPTY_BYTES, msg, &mut tag).unwrap();
-
     let mut output = Vec::with_capacity(AES_IV_LENGTH + AES_TAG_LENGTH + encrypted.len());
     output.extend(iv.iter());
     output.extend(tag.iter());
@@ -103,6 +82,14 @@ pub fn aes_decrypt(key: &[u8], encrypted_msg: &[u8]) -> Vec<u8> {
     decrypt_aead(cipher, key, Some(&iv), &EMPTY_BYTES, encrypted, tag).unwrap()
 }
 
+// private
+fn hkdf_sha256(master: &[u8]) -> [u8; 32] {
+    let h = Hkdf::<Sha256>::new(None, master);
+    let mut out = [0u8; 32];
+    h.expand(&EMPTY_BYTES, &mut out).unwrap();
+    out
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -117,14 +104,28 @@ mod tests {
     }
 
     #[test]
+    fn check_generate_keypair() {
+        let (sk1, pk1) = generate_keypair();
+        let (sk2, pk2) = generate_keypair();
+        assert_ne!(sk1, sk2);
+        assert_ne!(pk1, pk2);
+    }
+
+    #[test]
     fn check_aes_random_key() {
         let text = b"this is a text";
         let mut key = [0u8; 32];
-        OsRng.fill_bytes(&mut key);
+        thread_rng().fill(&mut key);
 
         assert_eq!(
             text,
             aes_decrypt(&key, aes_encrypt(&key, text).as_slice()).as_slice()
+        );
+
+        let utf8_text = "😀😀😀😀".as_bytes();
+        assert_eq!(
+            utf8_text,
+            aes_decrypt(&key, aes_encrypt(&key, utf8_text).as_slice()).as_slice()
         );
     }
 
@@ -145,9 +146,7 @@ mod tests {
     }
 
     #[test]
-    fn test_get_valid_secret() {
-        get_valid_secret();
-
+    fn test_valid_secret() {
         // 0 < private key < group order int is valid
         let zero = [0u8; 32];
         assert_eq!(
@@ -186,11 +185,10 @@ mod tests {
         two[31] = 2u8;
         three[31] = 3u8;
 
-        let secp = Secp256k1::new();
         let sk2 = SecretKey::from_slice(&two).unwrap();
-        let pk2 = PublicKey::from_secret_key(&secp, &sk2);
+        let pk2 = PublicKey::from_secret_key(&CONTEXT, &sk2);
         let sk3 = SecretKey::from_slice(&three).unwrap();
-        let pk3 = PublicKey::from_secret_key(&secp, &sk3);
+        let pk3 = PublicKey::from_secret_key(&CONTEXT, &sk3);
 
         assert_eq!(encapsulate(&sk2, &pk3), decapsulate(&pk2, &sk3));
         assert_eq!(
