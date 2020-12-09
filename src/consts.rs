fd65c52f7ec39ea4fd9f817c8e54c9e786f2b0a3
Weiliang Li <to.be.impressive@gmail.com>
2020-12-09 13:37:28 +0900

    revamp doc

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,8 @@
+/// AES IV/nonce length
 pub const AES_IV_LENGTH: usize = 16;
+/// AES tag length
 pub const AES_TAG_LENGTH: usize = 16;
+/// AES IV + tag length
 pub const AES_IV_PLUS_TAG_LENGTH: usize = AES_IV_LENGTH + AES_TAG_LENGTH;
+/// Empty bytes array
 pub const EMPTY_BYTES: [u8; 0] = [];
